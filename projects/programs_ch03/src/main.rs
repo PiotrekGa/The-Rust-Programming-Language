9c47e95120281392a5a973e19dd1909934fb121e
Piotr Gabrys <piotr.gabrys@allegrogroup.internal>
2020-05-14 13:21:38 +0200

    more fib

--- projects/programs_ch03/src/main.rs
+++ projects/programs_ch03/src/main.rs
@@ -8,6 +8,16 @@ fn main() {
     let fib = fib_num_calc(fib_num);
 
     println!("fib num for {} is {}", fib_num, fib);
+
+
+    println!("fib num for {} is {}", 2, fibonacci(2));
+    println!("fib num for {} is {}", 3, fibonacci(3));
+    println!("fib num for {} is {}", 4, fibonacci(4));
+    println!("fib num for {} is {}", 5, fibonacci(5));
+    println!("fib num for {} is {}", 6, fibonacci(6));
+    println!("fib num for {} is {}", 7, fibonacci(7));
+    println!("fib num for {} is {}", 8, fibonacci(8));
+
 }
 
 fn fahr_to_celc(deg: f32) -> f32 {
@@ -24,11 +34,19 @@ fn fib_num_calc(num: i32) -> i32 {
     } else if num == 1 {
         1
     } else {
-        for _ in 1..num {
+        for _ in 0..num {
             fib = fib0 + fib1;
             fib0 = fib1;
             fib1 = fib;
         }
         fib
+    }
+}
+
+fn fibonacci(n: u32) -> u32 {
+    match n {
+        0 => 1,
+        1 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
     }
 }
